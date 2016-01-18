c675e5552058acb70bb767705b9b759102022e2f
Florin Grozea <florin.grozea@fortech.ro>
2016-01-18 23:07:54 +0200

    clean up.

--- src/main/java/sample/config/WebSecurityConfig.java
+++ src/main/java/sample/config/WebSecurityConfig.java
@@ -38,6 +38,7 @@ public class WebSecurityConfig
     protected void configure(HttpSecurity http) throws Exception {
 
         http
+            /*.csrf().disable()*/
             .authorizeRequests()
                 .antMatchers("/resources/**", "/signup").permitAll()
                 .anyRequest().authenticated()
